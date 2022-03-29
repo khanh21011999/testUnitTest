e35afad69ed32879f133b2570cd7b3ccfaf2c7b3
Le Quoc Khanh <khanhlq21011999@gmail.com>
2022-03-29 10:46:32 +0700

    update

--- __TEST__/sum.test.js
+++ __TEST__/sum.test.js
@@ -1,5 +1,5 @@
-import FetchData from './fetchData';
-import sum from './sum';
+import FetchData from '../fetchData';
+import sum from '../sum';
 
 test('2+2 equal 4', () => {
   expect(sum(2, 2)).toBe(4);
